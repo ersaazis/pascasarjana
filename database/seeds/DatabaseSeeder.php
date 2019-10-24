b7ced2219d1329dc29b0455f5ba04ddfcb0fcbfe
ersaazis <eam24maret@gmail.com>
2019-10-25 01:54:47 +0700

    Update Seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -28,6 +28,5 @@ class DatabaseSeeder extends Seeder
         $this->call(CmsStatisticComponentsTableSeeder::class);
         $this->call(CmsStatisticsTableSeeder::class);
         $this->call(CmsUsersTableSeeder::class);
-        $this->call(KategoriDokumenTableSeeder::class);
     }
 }
